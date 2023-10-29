6c7b92858aedf10e5e4af88c9613394c074bddfa
Drouk1 <andy.orfanidis@gmail.com>
2023-10-29 21:16:59 +0200

    Initialize data in main method

--- erotima1/Main.java
+++ erotima1/Main.java
@@ -1,18 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-         // Check if no arguments were provided
-         if(args.length < 1) {
+        // Check if no arguments were provided
+        if(args.length < 1) {
             // Print an error message indicating the problem
             System.err.println("Error: Please provide the number of family members and their times.");
             // Exit the program with a status code indicating an error
             System.exit(1);
-        
-            // Check if the number of provided times (args.length - 1) doesn't match the number of family members specified in args[0]
-            }else if(args.length-1 != Integer.parseInt(args[0])) {
+        // Check if the number of provided times (args.length - 1) doesn't match the number of family members specified in args[0]
+        } else if(args.length-1 != Integer.parseInt(args[0])) {
             // Print an error message indicating the mismatch
             System.err.println("Error: The amount of family members given is not equal to the times given.");
             // Exit the program with a status code indicating an error
             System.exit(1);
-            }
+        }
+
+        // Initialize data
+        int[] times = new int[args.length - 1];
+        for (int i = 0; i < times.length; i++) {
+            times[i] = Integer.parseInt(args[i + 1]);
+        }
+
+        System.out.println(times);
     }
 }
