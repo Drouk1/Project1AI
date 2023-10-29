6832917417cb40bf38f1ec3afa3bac1075e3bbe8
Drouk1 <andy.orfanidis@gmail.com>
2023-10-29 20:55:59 +0200

    Making the state constructor

--- erotima1/State.java
+++ erotima1/State.java
@@ -1,25 +1,45 @@
 import java.util.*;
-
 public class State implements Comparable<State>
 {
 	private int f, h, g;
 	private State father;
 	private int totalTime;
+    private List<Integer> leftSide; // stores indices of people on the left side
+    private List<Integer> rightSide; // stores indices of people on the right side
+    private boolean torchPosition; // true if on the left side, false otherwise
+    private int[] times; // times required for each person to cross
+
 	
 	//constructor - fill with arguments if necessary
 	public State() 
 	{
 		this.f = 0;
-		this.h = 0;
-		this.g = 0;
-		this.father = null;
-		this.totalTime = 0;
+        this.h = 0;
+        this.g = 0;
+        this.father = null;
+        this.totalTime = 0;
+        this.leftSide = new ArrayList<>();
+        for (int i = 0; i < times.length; i++) {
+            this.leftSide.add(i); // initially, everyone is on the left side
+        }
+        this.rightSide = new ArrayList<>();
+        this.torchPosition = true; // torch starts on the left side
+        this.times = times;
 	}
 	
 	// copy constructor
 	public State(State s)
 	{
-		// create a state similar with s...
+		this.f = s.f;
+        this.g = s.g;
+        this.h = s.h;
+        this.father = s.father; // you might want a deep copy here depending on usage
+        this.totalTime = s.totalTime;
+        this.leftSide = new ArrayList<>(s.leftSide);
+        this.rightSide = new ArrayList<>(s.rightSide);
+        this.torchPosition = s.torchPosition;
+        this.times = s.times; // assuming this won't be modified, so no deep copy
+    }
 	}
 	
 	public int getF() 
